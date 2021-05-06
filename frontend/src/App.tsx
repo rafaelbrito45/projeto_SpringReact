7ec37c9a07ea28cebbf6f0c004fb26e5e949a105
rafaelbrito45 <rafael.rbrito45@gmail.com>
2021-05-05 23:05:58 -0300

    basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
-
-
-
+import DataTable from "components/dataTable"
+import Footer from "components/footer"
+import NavBar from "components/navBar"
 function App() {
   return (
-    <div>
+    <>
+    <NavBar/>
+    <div className="container">
       <h1 className="text-primary">Olá Mundo</h1>
+      <DataTable/>
     </div>
+    <Footer/>
+    </>
   );
 }
 
